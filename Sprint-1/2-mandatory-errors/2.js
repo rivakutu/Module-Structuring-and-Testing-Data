4cd4b54d14781941cb9dbc0e6f3475bc625cc004
rivakutu <revivemmapfumo@gmail.com>
2025-05-27 20:15:35 +0200

    declaration before using variable

--- Sprint-1/2-mandatory-errors/2.js
+++ Sprint-1/2-mandatory-errors/2.js
@@ -1,5 +1,6 @@
 // Currently trying to print the string "I was born in Bolton" but it isn't working...
 // what's the error ?
 
-console.log(`I was born in ${cityOfBirth}`);
+// the variabel cityOfBirth was used before being declared
 const cityOfBirth = "Bolton";
+console.log(`I was born in ${cityOfBirth}`);
