9e5818d8d83fbc0ce3eb76f5caeb8e407f0bb12d
rivakutu <revivemmapfumo@gmail.com>
2025-05-28 09:18:49 +0200

    updated variable names to allowed naming conventions

--- Sprint-1/2-mandatory-errors/4.js
+++ Sprint-1/2-mandatory-errors/4.js
@@ -1,2 +1,5 @@
-const 12HourClockTime = "20:53";
-const 24hourClockTime = "08:53";
+const TwelveHourClockTime = "20:53";
+const Twenty4hourClockTime = "08:53";
+
+//Numbers cannot be used in this manner to create variable names
+// if we want to use numbers we can use them at the end or in the middle of the variable name
