5ebfaf69661e608faf78c5a1672141718f91fd29
rivakutu <revivemmapfumo@gmail.com>
2025-05-28 12:26:43 +0200

    variable already declared

--- Sprint-2/1-key-errors/0.js
+++ Sprint-2/1-key-errors/0.js
@@ -1,13 +1,22 @@
 // Predict and explain first...
 //  =============> write your prediction here
+// there will be an error message because the function is not being called
+//
 
 // call the function capitalise with a string input
 // interpret the error message and figure out why an error is occurring
 
-function capitalise(str) {
+/*function capitalise(str) {
   let str = `${str[0].toUpperCase()}${str.slice(1)}`;
   return str;
 }
+*/
 
 // =============> write your explanation here
+// the error message shows that str has already been declared
 // =============> write your new code here
+function capitalise(str) {
+  let strr = `${str[0].toUpperCase()}${str.slice(1)}`;
+  return strr;
+}
+console.log(capitalise("orange"));
