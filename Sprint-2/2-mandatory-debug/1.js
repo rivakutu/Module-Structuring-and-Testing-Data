fb3dbbf3fb90c52056f8c994863a05415ff13efe
rivakutu <revivemmapfumo@gmail.com>
2025-05-29 01:13:35 +0200

    properly written return statement

--- Sprint-2/2-mandatory-debug/1.js
+++ Sprint-2/2-mandatory-debug/1.js
@@ -1,13 +1,20 @@
 // Predict and explain first...
 //  =============> write your prediction here
+//the program will run into an error because return statement
+// is not written properly
 
-function sum(a, b) {
+/*function sum(a, b) {
   return;
   a + b;
-}
+} */
 
 console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
 
 // =============> write your explanation here
+// the correct way of returning would be return a + b;
 // Finally, correct the code to fix the problem
 //  =============> write your new code here
+
+function sum(a, b) {
+  return a + b;
+}
