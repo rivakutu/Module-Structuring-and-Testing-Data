fd094151dc34859ad008a9ad2e0e3fc4c8af846f
rivakutu <revivemmapfumo@gmail.com>
2025-05-28 09:41:33 +0200

    adding comma to line 5 and answering Qs

--- Sprint-1/3-mandatory-interpret/1-percentage-change.js
+++ Sprint-1/3-mandatory-interpret/1-percentage-change.js
@@ -2,7 +2,7 @@ let carPrice = "10,000";
 let priceAfterOneYear = "8,543";
 
 carPrice = Number(carPrice.replaceAll(",", ""));
-priceAfterOneYear = Number(priceAfterOneYear.replaceAll("," ""));
+priceAfterOneYear = Number(priceAfterOneYear.replaceAll(",", ""));
 
 const priceDifference = carPrice - priceAfterOneYear;
 const percentageChange = (priceDifference / carPrice) * 100;
@@ -20,3 +20,15 @@ console.log(`The percentage change is ${percentageChange}`);
 // d) Identify all the lines that are variable declarations
 
 // e) Describe what the expression Number(carPrice.replaceAll(",","")) is doing - what is the purpose of this expression?
+
+//a) there are 4 function calls in this file,
+/* these are the function calls Number(carPrice.replaceAll(",", ""));-- first function call
+is Number() and second one is replaceAll() both on line 4
+third function call is Number() and replaceAll() which are both on line 5
+*/
+//b) the error is coming from line 5. it is occurring due to a missing comma after the first double
+// quote marks
+//c)line 4 and line 5 are variable reassignment statements
+//d)lines 1 and 2 are variable declarations
+//e) the expression is converting a string variable carPrice to an whole number using the Number() method
+// and by also using the replaceAll() method to replace "," with a space
