d3e9d91f8ffae6bfaed04f19b0e85e677e5e3f85
rivakutu <revivemmapfumo@gmail.com>
2025-05-28 12:52:41 +0200

    proper function definition and parameter parsing

--- Sprint-2/1-key-errors/2.js
+++ Sprint-2/1-key-errors/2.js
@@ -1,20 +1,27 @@
-
 // Predict and explain first BEFORE you run any code...
 
 // this function should square any number but instead we're going to get an error
 
 // =============> write your prediction of the error here
+// the 3 passed to the function will cause an error because we cannot use an integer
+// as a variable name. i think it could cause a type error or undefined error
 
-function square(3) {
+/*function square(3) {
     return num * num;
 }
+*/
 
 // =============> write the error message here
+// syntax error: unexpected number
 
 // =============> explain this error message here
+//An integer cannot be used as a parameter name
 
 // Finally, correct the code to fix the problem
 
 // =============> write your new code here
 
-
+function square(num) {
+  return num * num;
+}
+console.log(square(3));
