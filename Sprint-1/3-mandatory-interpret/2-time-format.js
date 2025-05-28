c3576b26dd0b49f299687e379f32274f60ca372f
rivakutu <revivemmapfumo@gmail.com>
2025-05-28 10:32:38 +0200

    movie duration calculator

--- Sprint-1/3-mandatory-interpret/2-time-format.js
+++ Sprint-1/3-mandatory-interpret/2-time-format.js
@@ -23,3 +23,14 @@ console.log(result);
 // e) What do you think the variable result represents? Can you think of a better name for this variable?
 
 // f) Try experimenting with different values of movieLength. Will this code work for all values of movieLength? Explain your answer
+
+//a) there are 5 variable declarations in this program namely movieLength, remainingSeconds,
+// totalMinutes, remainingMinutes and totalHours
+//b) there is only 1 function call which is on line 10
+//c) the expression movieLength % 60 means we are telling the computer to give us the remainder
+// of movieLength/60
+//d) the expression is for isolating total hours and minutes from the seconds to make the calculation
+// easier and precise and keeping the results as totalMinutes
+//e) the variable result represents the movie duration. A better name which keeps inline with
+// the variable names of this file would be movieDuration
+//f) yes the code will work for different values of movieLength
