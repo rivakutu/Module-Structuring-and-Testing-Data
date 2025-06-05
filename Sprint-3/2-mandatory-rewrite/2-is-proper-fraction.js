58113a5a135b37cb500f022baf04fccfca16b531
rivakutu <revivemmapfumo@gmail.com>
2025-06-05 17:48:23 +0200

    is proper fraction complete

--- Sprint-3/2-mandatory-rewrite/2-is-proper-fraction.js
+++ Sprint-3/2-mandatory-rewrite/2-is-proper-fraction.js
@@ -1,6 +1,10 @@
 function isProperFraction(numerator, denominator) {
-    if (numerator < denominator) return true;
-    // add your completed function from key-implement here
+  if (numerator < denominator) return true;
+  // add your completed function from key-implement here
+  if (numerator > denominator) return false;
+  if (Math.abs(numerator) < denominator) return true;
+  if (numerator === denominator) return false;
+  if (denominator === 0) return false;
 }
 
 module.exports = isProperFraction;
