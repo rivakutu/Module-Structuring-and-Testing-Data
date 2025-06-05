4c09bc36a0bf709fed12079892e422670dbbe234
rivakutu <revivemmapfumo@gmail.com>
2025-06-05 18:51:02 +0200

    count tests completed

--- Sprint-3/3-mandatory-practice/implement/count.test.js
+++ Sprint-3/3-mandatory-practice/implement/count.test.js
@@ -22,3 +22,9 @@ test("should count multiple occurrences of a character", () => {
 // And a character char that does not exist within the case-sensitive str,
 // When the function is called with these inputs,
 // Then it should return 0, indicating that no occurrences of the char were found in the case-sensitive str.
+test("should check for no occurrence characters", () => {
+  const str = "aaaaa";
+  const char = "b";
+  const count = countChar(str, char);
+  expect(count).toEqual(0);
+});
