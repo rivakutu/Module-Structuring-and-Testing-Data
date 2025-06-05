2a6afb4f7792f57681ddf84b55c98200f6318be9
rivakutu <revivemmapfumo@gmail.com>
2025-06-05 19:04:37 +0200

    get ordinal number completed

--- Sprint-3/3-mandatory-practice/implement/get-ordinal-number.js
+++ Sprint-3/3-mandatory-practice/implement/get-ordinal-number.js
@@ -1,5 +1,8 @@
 function getOrdinalNumber(num) {
- return "1st";
+  if (num === 1) return "1st";
+  if (num === 2) return "2nd";
+  if (num === 3) return "3rd";
+  if (num === 4) return "4th";
 }
 
 module.exports = getOrdinalNumber;
