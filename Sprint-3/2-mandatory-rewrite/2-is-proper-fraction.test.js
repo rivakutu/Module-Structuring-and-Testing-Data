02cc899bb461b4acc394bebc158875d55a7fb7be
rivakutu <revivemmapfumo@gmail.com>
2025-06-05 17:48:41 +0200

    is proper fraction test complete

--- Sprint-3/2-mandatory-rewrite/2-is-proper-fraction.test.js
+++ Sprint-3/2-mandatory-rewrite/2-is-proper-fraction.test.js
@@ -5,7 +5,16 @@ test("should return true for a proper fraction", () => {
 });
 
 // Case 2: Identify Improper Fractions:
+test("should return false for improper fractions", () => {
+  expect(isProperFraction(3, 2)).toEqual(false);
+});
 
 // Case 3: Identify Negative Fractions:
+test("should return true for negative fractions", () => {
+  expect(isProperFraction(-2, 3)).toEqual(true);
+});
 
 // Case 4: Identify Equal Numerator and Denominator:
+test("should return false for equal numerator and denominator", () => {
+  expect(isProperFraction(2, 2)).toEqual(false);
+});
