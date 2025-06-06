1ab59d34cf206ecf65fc7737179110f87c654dff
rivakutu <revivemmapfumo@gmail.com>
2025-06-06 19:27:09 +0200

    Clarifies the operation of the increment statement

--- Sprint-1/1-key-exercises/1-count.js
+++ Sprint-1/1-key-exercises/1-count.js
@@ -4,4 +4,4 @@ count = count + 1;
 
 // Line 1 is a variable declaration, creating the count variable with an initial value of 0
 // Describe what line 3 is doing, in particular focus on what = is doing
-// line 3 is incrementing the value of count by 1
+// line 3 is incrementing the value of count by 1 and = is being used to update count
