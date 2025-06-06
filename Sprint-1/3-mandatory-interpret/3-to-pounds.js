7bc0ca6e8685e48f87fd6dfea9670c4c88d6b77b
rivakutu <revivemmapfumo@gmail.com>
2025-06-06 18:07:21 +0200

    Explains logic for pence to pounds conversion

--- Sprint-1/3-mandatory-interpret/3-to-pounds.js
+++ Sprint-1/3-mandatory-interpret/3-to-pounds.js
@@ -25,3 +25,10 @@ console.log(`£${pounds}.${pence}`);
 
 // To begin, we can start with
 // 1. const penceString = "399p": initialises a string variable with the value "399p"
+// 2. const penceStringWithoutTrailingP = penceString.substring(0,penceString.length - 1); removes the last character from the string
+// const paddedPenceNumberString = penceStringWithoutTrailingP.padStart(3, "0"); this will add zeros at the beginning of the string to
+// make sure its 3 characters long
+// 3. const pounds = paddedPenceNumberString.substring(0,paddedPenceNumberString.length - 2); this will remove the last 2 digits
+// from the string to come up with the pound value
+// 4. const pence = paddedPenceNumberString.substring(paddedPenceNumberString.length - 2).padEnd(2, "0"); this will extract the last two
+// characters of the string and adds a zero if it is less than 2 characters long
