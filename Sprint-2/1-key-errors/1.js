92305476a43497cfdfac69ac895ffa3a3c199e7d
rivakutu <revivemmapfumo@gmail.com>
2025-06-06 17:12:41 +0200

    Fixes variable scope issue

--- Sprint-2/1-key-errors/1.js
+++ Sprint-2/1-key-errors/1.js
@@ -1,11 +1,11 @@
 // Predict and explain first...
 
 // Why will an error occur when this program runs?
-// =============> write your prediction here
+// =============> An error will occur because decimalNumber is declared twice
 
 // Try playing computer with the example to work out what is going on
 
-function convertToPercentage(decimalNumber) {
+/*function convertToPercentage(decimalNumber) {
   const decimalNumber = 0.5;
   const percentage = `${decimalNumber * 100}%`;
 
@@ -13,8 +13,17 @@ function convertToPercentage(decimalNumber) {
 }
 
 console.log(decimalNumber);
+*/
 
-// =============> write your explanation here
+// =============> An error will occur because decimalNumber is declared twice
+// and decimalNumber is given as input but also is assigned a value which is problematic
 
 // Finally, correct the code to fix the problem
 // =============> write your new code here
+function convertToPercentage(decimalNumber) {
+  const percentage = `${decimalNumber * 100}%`;
+
+  return percentage;
+}
+
+console.log(convertToPercentage(0.5));
