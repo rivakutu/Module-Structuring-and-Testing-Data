658adb22eac4e26ae0973c2882e33274f678e717
rivakutu <revivemmapfumo@gmail.com>
2025-06-06 17:49:03 +0200

    Adds toUpperSnakeCase function

--- Sprint-2/3-mandatory-implement/2-cases.js
+++ Sprint-2/3-mandatory-implement/2-cases.js
@@ -14,3 +14,9 @@
 // You will need to come up with an appropriate name for the function
 // Use the MDN string documentation to help you find a solution
 // This might help https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/toUpperCase
+
+function toUpperSnakeCase(str) {
+  return str.toUpperCase().replace(/\s+/g, "_");
+}
+
+console.log(toUpperSnakeCase("bat man caught the joker"));
