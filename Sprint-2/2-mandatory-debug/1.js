2145756210eb96b3d8979285f5324f9bdea3940d
rivakutu <revivemmapfumo@gmail.com>
2025-06-06 17:22:41 +0200

    Fixes function to return the sum of two numbers

--- Sprint-2/2-mandatory-debug/1.js
+++ Sprint-2/2-mandatory-debug/1.js
@@ -1,13 +1,19 @@
 // Predict and explain first...
-//  =============> write your prediction here
+//  =============> an error will the thrown because there is nothing after the return statement
 
-function sum(a, b) {
+/*function sum(a, b) {
   return;
   a + b;
 }
 
 console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
-
-// =============> write your explanation here
+*/
+// =============> we should return a+ b to get the desired results
 // Finally, correct the code to fix the problem
 //  =============> write your new code here
+
+function sum(a, b) {
+  return a + b;
+}
+
+console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
