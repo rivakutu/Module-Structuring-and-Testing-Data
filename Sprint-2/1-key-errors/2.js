979d54d609894be0deaf677638cd430307cd297b
rivakutu <revivemmapfumo@gmail.com>
2025-06-06 17:16:53 +0200

    Fixes syntax error in square function

--- Sprint-2/1-key-errors/2.js
+++ Sprint-2/1-key-errors/2.js
@@ -1,20 +1,21 @@
-
 // Predict and explain first BEFORE you run any code...
 
 // this function should square any number but instead we're going to get an error
 
-// =============> write your prediction of the error here
+// =============> the function was given 3 as input instead of num. this will cause an error
 
-function square(3) {
+/*function square(3) {
     return num * num;
 }
+*/
+// =============> SyntaxError
 
-// =============> write the error message here
-
-// =============> explain this error message here
+// =============> The error means we are not following javascript programming rules
 
 // Finally, correct the code to fix the problem
 
 // =============> write your new code here
-
-
+function square(num) {
+  return num * num;
+}
+console.log(square(3));
