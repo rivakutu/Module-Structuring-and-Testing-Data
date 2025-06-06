5d93014517aa2a3a70342a2b2826c389303b1438
rivakutu <revivemmapfumo@gmail.com>
2025-06-06 17:20:46 +0200

    Fixes function to return the multiplication result

--- Sprint-2/2-mandatory-debug/0.js
+++ Sprint-2/2-mandatory-debug/0.js
@@ -1,14 +1,20 @@
 // Predict and explain first...
 
-// =============> write your prediction here
+// =============> the function is not returning anything so there will be an error
 
-function multiply(a, b) {
+/*function multiply(a, b) {
   console.log(a * b);
 }
 
 console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
-
-// =============> write your explanation here
+*/
+// =============> we should add a return statement in the function to get desired results
 
 // Finally, correct the code to fix the problem
 //  =============> write your new code here
+
+function multiply(a, b) {
+  return a * b;
+}
+
+console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
