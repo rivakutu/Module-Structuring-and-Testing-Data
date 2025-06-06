881c0594a8ee7e1542939510b7658af666753d16
rivakutu <revivemmapfumo@gmail.com>
2025-06-06 17:01:07 +0200

    0js capitalise

--- Sprint-2/1-key-errors/0.js
+++ Sprint-2/1-key-errors/0.js
@@ -1,13 +1,20 @@
 // Predict and explain first...
-//  =============> write your prediction here
+//  =============> given a string as input the function will capitalize the first letter then
+// add it to a sliced version of the original string starting at index 0 until the end of the string
 
 // call the function capitalise with a string input
 // interpret the error message and figure out why an error is occurring
 
-function capitalise(str) {
+/*function capitalise(str) {
   let str = `${str[0].toUpperCase()}${str.slice(1)}`;
   return str;
 }
-
-// =============> write your explanation here
+*/
+// =============> the error is due to declaring str inside the function when it has already passed as input for the function
 // =============> write your new code here
+
+function capitalise(str) {
+  str = `${str[0].toUpperCase()}${str.slice(1)}`;
+  return str;
+}
+console.log(capitalise("person"));
