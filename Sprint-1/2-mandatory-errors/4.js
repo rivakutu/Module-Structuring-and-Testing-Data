ae0c08a865a178fbda0ee07c95ae027682010a7f
rivakutu <revivemmapfumo@gmail.com>
2025-06-07 15:14:25 +0200

    Fixes invalid variable names

--- Sprint-1/2-mandatory-errors/4.js
+++ Sprint-1/2-mandatory-errors/4.js
@@ -1,2 +1,4 @@
-const 12HourClockTime = "20:53";
-const 24hourClockTime = "08:53";
+const TwelveHourClockTime = "20:53";
+const TwentyFourHourClockTime = "08:53";
+
+// Syntax error: variables were named incorrectly-- variable names cannot start with a number
