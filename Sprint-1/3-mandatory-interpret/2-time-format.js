36fa5cee3597b76b30b4f83dd0e64c791989f0ba
rivakutu <revivemmapfumo@gmail.com>
2025-06-07 16:34:00 +0200

    Documents variable declarations and functions

--- Sprint-1/3-mandatory-interpret/2-time-format.js
+++ Sprint-1/3-mandatory-interpret/2-time-format.js
@@ -12,14 +12,20 @@ console.log(result);
 // For the piece of code above, read the code and then answer the following questions
 
 // a) How many variable declarations are there in this program?
+// there are 4 variable declarations in this program
 
 // b) How many function calls are there?
+// there 0 function calls in this program
 
 // c) Using documentation, explain what the expression movieLength % 60 represents
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Arithmetic_Operators
+// this expression gives the remainder of movieLength/60
 
 // d) Interpret line 4, what does the expression assigned to totalMinutes mean?
+// this represents the total number of minutes
 
 // e) What do you think the variable result represents? Can you think of a better name for this variable?
+// the value results represents the movie length. An appropriate name would be movieDuration
 
 // f) Try experimenting with different values of movieLength. Will this code work for all values of movieLength? Explain your answer
+// yes the code will work for different values of movieLength because the logic is sound
