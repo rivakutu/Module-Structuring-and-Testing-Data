cf2adac23d5aae17bbd167e6916a661b92d3026b
rivakutu <revivemmapfumo@gmail.com>
2025-06-07 15:02:03 +0200

    Comments out instructions.

--- Sprint-1/2-mandatory-errors/0.js
+++ Sprint-1/2-mandatory-errors/0.js
@@ -1,2 +1,3 @@
-This is just an instruction for the first activity - but it is just for human consumption
-We don't want the computer to run these 2 lines - how can we solve this problem?
+//This is just an instruction for the first activity - but it is just for human consumption
+//We don't want the computer to run these 2 lines - how can we solve this problem?
+// I added double forward slashes to comment the text for human consumption
