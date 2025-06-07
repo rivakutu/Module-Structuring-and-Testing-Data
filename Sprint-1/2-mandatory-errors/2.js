84fd08891080a5b0275e9941d976feeaa2220691
rivakutu <revivemmapfumo@gmail.com>
2025-06-07 15:06:45 +0200

    Fixes variable hoisting issue

--- Sprint-1/2-mandatory-errors/2.js
+++ Sprint-1/2-mandatory-errors/2.js
@@ -1,5 +1,6 @@
 // Currently trying to print the string "I was born in Bolton" but it isn't working...
 // what's the error ?
+// the error is due to logging before variable assignment
 
-console.log(`I was born in ${cityOfBirth}`);
 const cityOfBirth = "Bolton";
+console.log(`I was born in ${cityOfBirth}`);
