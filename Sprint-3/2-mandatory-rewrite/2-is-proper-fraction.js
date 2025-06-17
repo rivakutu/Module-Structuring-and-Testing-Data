badfd941f9178979ec896b040073f63ea2b4d936
rivakutu <revivemmapfumo@gmail.com>
2025-06-17 14:41:03 +0200

    minor if statement appearing adjustment

--- Sprint-3/2-mandatory-rewrite/2-is-proper-fraction.js
+++ Sprint-3/2-mandatory-rewrite/2-is-proper-fraction.js
@@ -1,5 +1,7 @@
 function isProperFraction(numerator, denominator) {
-  if (denominator === 0) throw new Error("Denominator cannot be 0");
+  if (denominator === 0) {
+    throw new Error("Denominator cannot be 0");
+  }
   const absoluteNumerator = Math.abs(numerator);
   const absoluteDenominator = Math.abs(denominator);
   return absoluteNumerator < absoluteDenominator;
