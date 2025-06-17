934f97037ad8b9501c02ff01721dde6b115c78df
rivakutu <revivemmapfumo@gmail.com>
2025-06-17 15:42:09 +0200

    explaining the importance of index++

--- Sprint-3/4-stretch-investigate/find.js
+++ Sprint-3/4-stretch-investigate/find.js
@@ -24,6 +24,8 @@ console.log(find("code your future", "z"));
 // b) What is the if statement used to check
 // when index reaches desired char return it
 // c) Why is index++ being used?
-//idex++ is being used to add 1 to the value of index
+//idex++ is being used to increment the index by one on each iteration of the loop
+// this ensures the loop progresses through the string, moving from one position to the next
+//without the increment the loop will be stuck on an infinite loop and on the same index
 // d) What is the condition index < str.length used for?
 //this ensures the index does not go beyond the length of the string
